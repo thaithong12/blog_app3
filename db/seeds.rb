7199695ca2692789736d46722d8eeca967ddcbde
thaithong12 <nguyenthaithong98@gmail.com>
2020-06-11 15:56:20 +0700

    [1.0.8] Check Register

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,15 @@ end
     name = Faker::Job.education_level
     Category.create!(category_name: name)
 end
+
+users =User.order(:created_at).take(6)
+30.times do  
+	description =Faker::Lorem.sentence(50)  
+	book_name = Faker::Job.field 
+	author = Faker::Artist.name
+	publish_date = Time.now
+	rating = Faker::Number.between(from: 1, to: 5) #=> 7
+	status = Faker::Number.between(from: 0, to: 1)
+	category_id = Faker::Number.between(from: 1, to: 10) 
+	users.each {|user| user.books.create!(description: description,book_name: book_name,author: author, publish_date: publish_date,rating: rating,status: status,category_id: category_id ) }
+end
