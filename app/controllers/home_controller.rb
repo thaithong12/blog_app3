d94784cdb57a71ec68e45e99829dd9329d2f5837
thaithong12 <nguyenthaithong98@gmail.com>
2020-06-13 10:59:09 +0700

    [1.1.4] Finish About

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,17 @@ class HomeController < ApplicationController
       @books_new =  Book.all.by_created_at_desc.paginate(page: params[:page], per_page: 10)
   end
 
+  def about 
+    
+  end
   private
   def check_login
     return if current_user
 			flash[:danger] = "You need Login !"
 			redirect_to login_path
-	end
+  end
+  
+
+ 
+  
 end
